008e4fa5f9f1607eec40484525b482270c92e3b6
Ogunfeitimi Olaniyi Michael <119413662+Mlekins@users.noreply.github.com>
2023-10-06 03:16:41 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Description: using sizeof to get various types sizes
@@ -11,12 +11,12 @@
 int main(void)
 {
 
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	prinf("Size of an int: %d byte(s)\n", sizeof(int));
-	prntf("Size of a double: %d byte(s)\n", sizeof(double));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+prinf("Size of an int: %d byte(s)\n", sizeof(int));
+prntf("Size of a double: %d byte(s)\n", sizeof(double));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	return (0);
+	return(0);
 }
