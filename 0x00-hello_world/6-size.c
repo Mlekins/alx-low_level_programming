42ae06b979d54929cbf35eaf082d080a78c6fd39
Ogunfeitimi Olaniyi Michael <119413662+Mlekins@users.noreply.github.com>
2023-10-06 03:53:47 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,12 @@
 int main(void)
 {
 
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-prinf("Size of an int: %lu byte(s)\n", sizeof(int));
-prntf("Size of a double: %lu byte(s)\n", sizeof(double));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+prinf("Size of an int: %zu byte(s)\n", sizeof(int));
+prntf("Size of a double: %zu byte(s)\n", sizeof(double));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)", sizeof(float));
 
 	return (0);
 }
