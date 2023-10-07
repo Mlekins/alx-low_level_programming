1a6ec0e047777925304a623a1ca0182215959296
Mlekins <michaelola97@gmail.com>
2023-10-07 10:20:05 -0700

    more c tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ *
+ * Return: Always 0 (Success)
+ */
+
+char letter;
+
+int main(void)
+{
+	for (letter = 'a'; letter <= 'z'; letter ++)
+	{
+		putchar (letter);
+	}
+
+	return 0;
+}
+	
