ec6229e1b5ed8063806c101c6d3eef9f37bfa733
Mlekins <michaelola97@gmail.com>
2023-10-08 17:03:53 -0700

    jti

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: print a quote using write function 
+ * Description: print a quote using write function
  *
  * Return: This time we return an error 1.
  */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
+	write(2,
+		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+		59);
 
-	write(1, quo, 59);
 	return (1);
 }
