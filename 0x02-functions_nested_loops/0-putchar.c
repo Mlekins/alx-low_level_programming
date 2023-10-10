6d61c2a022882f779ed4576fa29c8b477089fa28
Mlekins <michaelola97@gmail.com>
2023-10-10 12:17:05 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+
+	return (0);
+}
