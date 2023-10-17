dba16666b05b59f6044839e1021b1b7aae2e8e49
Mlekins <michaelola97@gmail.com>
2023-10-16 18:34:05 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts- a function that prints a string followed by a new line
+ * @str: string parameter
+ */
+
+void _puts(char *str)
+{
+	_putchar[](str);
+	_putchar('\n');
+}
