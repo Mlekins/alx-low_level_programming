f568049aaf0b555d8a638ed9d9037315c80959b1
Mlekins <michaelola97@gmail.com>
2023-10-16 18:57:30 -0700

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev- a function that prints a string in reverse followed by a new line
+ * @s: string input
+ */
+
+void print_rev(char *s)
+{
+	int rev;
+
+	/*finds the length of string without null character*/
+	for (rev = 0; s[rev] != '\0'; ++rev)
+		;
+
+	/*print char from the last indec as you decrement*/
+	for (--rev; rev >= 0; --rev)
+		_putchar(s[rev]);
+	_putchar('\n')
+}
